ec30f005aa82aa49c3e0f31ff601c21f44812e4c
Matt Stratton <matt.stratton@gmail.com>
2016-11-16 14:34:08 -0600

    Debug build script

--- bin/netlify.sh
+++ bin/netlify.sh
@@ -1,5 +1,8 @@
 pwd
 ls
+ln -s /opt/build/repo /opt/build/devopsdays-theme
+cd ..
+ls
 cd exampleSite
 ../bin/hugo version
 ../bin/hugo -v --theme=devopsdays-theme --buildDrafts=false --baseURL=$URL
