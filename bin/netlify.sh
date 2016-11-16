dc55e2fbeedfcb3585da3596668a3007267107f9
Matt Stratton <matt.stratton@gmail.com>
2016-11-16 14:35:37 -0600

    Fix build script

--- bin/netlify.sh
+++ bin/netlify.sh
@@ -1,8 +1,4 @@
-pwd
-ls
 ln -s /opt/build/repo /opt/build/devopsdays-theme
-cd ..
-ls
 cd exampleSite
 ../bin/hugo version
 ../bin/hugo -v --theme=devopsdays-theme --buildDrafts=false --baseURL=$URL
