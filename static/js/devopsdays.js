b80525593b267f2793e926ee3e9876838d9972e3
Matt Stratton <matt.stratton@gmail.com>
2017-01-10 18:40:28 -0800

    Clean up javascript
    Signed-off-by: Matt Stratton <matt.stratton@gmail.com>

--- static/js/devopsdays.js
+++ static/js/devopsdays.js
@@ -4,18 +4,22 @@
 // @codekit-prepend '../../bower_components/jquery-ui/jquery-ui.js'
 // @codekit-prepend '../../bower_components/markerwithlabel-v3/markerwithlabel.js'
 
-
 // accordion
-$( "#accordion" ).accordion(); // TODO: should only be included on program pages
+$(document).ready(function(){
+  "use strict";
+ jQuery( "#accordion" ).accordion();
+ });
 
 // embed stuff for talks
 
-$(function() {
+ jQuery(function() {
+   "use strict";
     $("a.embed").oembed();
 });
 
 // google maps
 (function() {
+  "use strict";
     var cx = '008769174023796316711:pj3syggtkz0';
     var gcse = document.createElement('script');
     gcse.type = 'text/javascript';
@@ -25,33 +29,38 @@ $(function() {
     s.parentNode.insertBefore(gcse, s);
 })();
 
-// stuff to highlight current month
-
-var now = new Date;
-
-$('.left-nav-months').text(function (i, v) {
-
-if (getMonth(now) == v) {
-  $(this).addClass('current');
-}
-
-});
-
 function getMonth(d) {
-var dt = new Date(d);
-var dtm = dt.getMonth();
-var month = new Array();
-month[0] = "January";
-month[1] = "February";
-month[2] = "March";
-month[3] = "April";
-month[4] = "May";
-month[5] = "June";
-month[6] = "July";
-month[7] = "August";
-month[8] = "September";
-month[9] = "October";
-month[10] = "November";
-month[11] = "December";
-return month[dtm]
+  "use strict";
+    var dt = new Date(d);
+    var dtm = dt.getMonth();
+    var month = [];
+    month[0] = "January";
+    month[1] = "February";
+    month[2] = "March";
+    month[3] = "April";
+    month[4] = "May";
+    month[5] = "June";
+    month[6] = "July";
+    month[7] = "August";
+    month[8] = "September";
+    month[9] = "October";
+    month[10] = "November";
+    month[11] = "December";
+    return month[dtm];
 }
+
+// stuff to highlight current month
+(function() {
+  "use strict";
+    var now = new Date();
+
+    $('.left-nav-months').text(function (i, v) {
+
+    if (getMonth(now) == v) {
+      $(this).addClass('current');
+    }
+
+  });
+
+
+})();
