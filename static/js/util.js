c0ca406215612f9d5d080da3e42eca4efa1800ca
Matt Stratton <matt.stratton@gmail.com>
2017-01-11 23:53:05 -0800

    Remove jquery-ui version of accordion (#100)
    Much simpler this way!
    
    Signed-off-by: Matt Stratton <matt.stratton@gmail.com>

--- static/js/util.js
+++
@@ -1,5 +0,0 @@
-// @codekit-prepend '../../bower_components/jquery/dist/jquery.js'
-// @codekit-prepend '../../bower_components/bootstrap/dist/js/bootstrap.js'
-// @codekit-prepend '../../bower_components/jquery-oembed-all/jquery.oembed.js'
-// @codekit-prepend './google-maps.js'
-// @codekit-prepend '../../bower_components/markerwithlabel-v3/markerwithlabel.js'
