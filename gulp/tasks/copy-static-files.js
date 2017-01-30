515682d949d3efd02bc9e8756b18e9d46f5cc548
Matt Stratton <matt.stratton@gmail.com>
2017-01-30 17:46:05 -0600

    Add file copy actions
    Signed-off-by: Matt Stratton <matt.stratton@gmail.com>

--- gulp/tasks/copy-static-files.js
+++ gulp/tasks/copy-static-files.js
@@ -1,6 +1,23 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    runSequence = require('run-sequence');
 
-gulp.task('copy-static-files', function(){
-  return gulp.src(['staging/sitemap.xml', 'staging/tags/**/*.xml','public/favicon*', 'public/apple-icon*', 'public/android-icon*', 'public/ms-icon*', 'public/manifest.json', 'public/browserconfig.xml', 'public/**/*.pdf','public/fonts/**/*.*', 'public/css/**/*.map'])
-  .pipe(gulp.dest('dist'));
+gulp.task('copy-misc-files', function() {
+    return gulp.src(['staging/sitemap.xml', 'staging/tags/**/*.xml', 'public/favicon*', 'public/apple-icon*', 'public/android-icon*', 'public/ms-icon*', 'public/manifest.json', 'public/browserconfig.xml', 'public/**/*.pdf'])
+        .pipe(gulp.dest('dist'));
 });
+
+gulp.task('copy-fonts', function() {
+    return gulp.src('public/fonts/**/*.*')
+        .pipe(gulp.dest('dist'));
+})
+
+gulp.task('copy-css-maps', function() {
+    return gulp.src('public/css/**/*.map')
+        .pipe(gulp.dest('dist'));
+})
+
+gulp.task('copy-static-files', function(callback) {
+    runSequence('copy-misc-files', 'copy-fonts', 'copy-css-maps',
+        callback
+    )
+})
