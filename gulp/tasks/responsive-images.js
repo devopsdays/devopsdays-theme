e8c2ee6dbe980a88f3d6939b87274d26f96141cf
Matt Stratton <matt.stratton@gmail.com>
2017-02-20 00:59:38 -0600

    Update image processing#252 (#256)
    * Make event nav look pretty on small screens. Fixes #185
    
    * Update gulp image processing

--- gulp/tasks/responsive-images.js
+++ gulp/tasks/responsive-images.js
@@ -1,12 +1,57 @@
 var gulp = require('gulp'),
     responsive = require('gulp-responsive');
 
+
 gulp.task('responsive-images', function() {
     return gulp.src(['public/**/*.png', 'public/**/*.jpg',
             '!public/favicon*', '!public/apple-icon*', '!public/android-icon*', '!public/ms-icon*', '!public/**/sharing.jpg'
         ])
         .pipe(responsive({
             // produce multiple images from one source
+
+            '**/logo_square.jpg': [{
+                width: '250px',
+                quality: 80
+            }, {
+                width: '500px',
+                quality: 80,
+                rename: {
+                    suffix: '@2x'
+                }
+            }, {
+                width: '1500px',
+                quality: 80,
+                rename: {
+                    suffix: '@3x'
+                }
+            }],
+
+            '**/logo.*': [{
+                width: '50%'
+            }, {
+                width: '100%',
+                rename: {
+                    suffix: '@2x'
+                }
+            }, {
+                width: '150%',
+                rename: {
+                    suffix: '@3x'
+                }
+            }],
+            '**/img/sponsors/*.*': [{
+                width: '100px'
+            }, {
+                width: '200px',
+                rename: {
+                    suffix: '@2x'
+                }
+            }, {
+                width: '300px',
+                rename: {
+                    suffix: '@3x'
+                }
+            }],
             '**/*.png': [{
                 width: '50%'
             }, {
