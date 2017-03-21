6948ae26cc872dd3cf6fb43c475ecf31e3dc90ff
Matt Stratton <matt.stratton@gmail.com>
2017-03-21 18:51:57 -0500

    Add baseurl because sitemap is bad new bears

--- bin/netlify-production.sh
+++ bin/netlify-production.sh
@@ -1,4 +1,4 @@
 ln -s /opt/build/repo /opt/build/devopsdays-theme
 cd exampleSite
-hugo_0.18 --theme=devopsdays-theme --buildDrafts=false --baseURL="/"
+hugo_0.19 --theme=devopsdays-theme --buildDrafts=false --baseURL="https://dev.devopsdays.org"
 gulp --cwd .
