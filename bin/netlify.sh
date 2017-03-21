44d4d4c927d08a79de828ad203697b67ecb29d3f
Matt Stratton <matt.stratton@gmail.com>
2017-03-21 18:52:11 -0500

    Update netlify.sh

--- bin/netlify.sh
+++ bin/netlify.sh
@@ -1,3 +1,3 @@
 ln -s /opt/build/repo /opt/build/devopsdays-theme
 cd exampleSite
-hugo_0.18 --theme=devopsdays-theme --buildDrafts=false --baseURL="/"
+hugo_0.19 --theme=devopsdays-theme --buildDrafts=false --baseURL="/"
