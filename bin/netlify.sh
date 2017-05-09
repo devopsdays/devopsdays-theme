7fb77909f5be0e040f4fc8dadb7ee66f1e00d349
Matt Stratton <matt.stratton@gmail.com>
2017-05-08 20:52:48 -0500

    Update hugo version to 0.20.7

--- bin/netlify.sh
+++ bin/netlify.sh
@@ -1,3 +1,3 @@
 ln -s /opt/build/repo /opt/build/devopsdays-theme
 cd exampleSite
-hugo_0.19 --theme=devopsdays-theme --buildDrafts=false --baseURL="$DEPLOY_PRIME_URL"
+hugo --theme=devopsdays-theme --buildDrafts=false --baseURL="$DEPLOY_PRIME_URL"
