c4e3ab2c7b588dd14bb8901b0f3f54dfea329796
Matt Stratton <matt.stratton@gmail.com>
2017-05-12 17:32:29 -0500

    Update netlify-production.sh

--- bin/netlify-production.sh
+++ bin/netlify-production.sh
@@ -1,6 +1,6 @@
 ln -s /opt/build/repo /opt/build/devopsdays-theme
 cd exampleSite
 
-../bin/hugo/version
-../bin/hugo --theme=devopsdays-theme --buildDrafts=false --baseURL="$URL"
+hugo version
+hugo --theme=devopsdays-theme --buildDrafts=false --baseURL="$URL"
 gulp --cwd .
